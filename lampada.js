e104551de5f1d13e7b98e648e0cc15a9fd820398
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-09-07 08:46:42 -0400

    criando a logica da lampada quebrada

--- lampada.js
+++ lampada.js
@@ -3,14 +3,19 @@ const turnOff = document.querySelector("#turnOff");
 const lamp = document.querySelector("#lamp");
 
 const lampOn = () => {
-    lamp.src = './img/ligada.jpg'
+    lamp.src = './img/ligada.jpg';
 };
 
 const lampOff = () => {
-    lamp.src = './img/desligada.jpg'
+    lamp.src = './img/desligada.jpg';
+};
+
+const lampBroken = () => {
+    lamp.src = './img/quebrada.jpg';
 };
 
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
